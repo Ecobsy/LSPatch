319dbfd79654ae455fbfc137945552ffec1be860
EduardoA3677 <ealvarado261500@gmail.com>
2025-06-14 00:35:03 +0000

    Refactor LSPTheme to replace ViewCompat with WindowCompat for status bar color handling

--- manager/src/main/java/org/lsposed/lspatch/ui/theme/Theme.kt
+++ manager/src/main/java/org/lsposed/lspatch/ui/theme/Theme.kt
@@ -9,7 +9,7 @@ import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 
 @Composable
 fun LSPTheme(
@@ -28,8 +28,9 @@ fun LSPTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.background.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = !isDarkTheme
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.surface.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !isDarkTheme
         }
     }
 
